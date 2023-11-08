eccc714b1f58cc9172deabb402e2de339d2d3390
Ramon Egger <ramon@ramonegger.ch>
2023-11-08 11:55:26 +0100

    removed unneeded variables

--- clone.sh
+++ clone.sh
@@ -3,10 +3,7 @@
 echo "Cloning repositories..."
 
 CODE=$HOME/Code
-SITES=$HOME/Herd
-BLADE=$CODE/blade-ui-kit
-LARAVEL=$CODE/laravel
 
 # Sites
-git clone git@github.com:datascaled/datascale-website.git $SITES/datascale-website
-git clone git@github.com:datascaled/orthopodo-website.git $SITES/orthopodo-website
+git clone git@github.com:datascaled/datascale-website.git $CODE/datascale-website
+git clone git@github.com:datascaled/orthopodo-website.git $CODE/orthopodo-website
